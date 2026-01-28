3884ec75622e259fa4bfcf6f9305786d7d9a335d
plattine <loghindan@gmail.com>
2026-01-28 16:15:27 +0100

    2nd commit

--- src/Lesson01/MyFirstJavaCode.java
+++ src/Lesson01/MyFirstJavaCode.java
@@ -4,6 +4,7 @@ public class MyFirstJavaCode {
 
     public static void main(String[] args){
         System.out.println("Hello, this is my first code!");
+        System.out.println("...and this is my second line of code!");
     }
 
 }
